ff830a8d2f8313ba5cdd9dffe6e057494753b031
mattsak <87535803+mattsak@users.noreply.github.com>
2023-03-22 17:25:18 -0400

    Update dataVisualization.py

--- dataVisualization.py
+++ dataVisualization.py
@@ -1,11 +1,3 @@
-'''
-Linux Commands:
-
-g++ log_parser.cpp -o log_parser
-
-./log_parser output.dat
-'''
-
 # Import Pandas Library to create a data frame and seaborn library for visualization tools
 import seaborn as sb
 import pandas as pd
@@ -17,20 +9,27 @@ def dataImport(textfile):
 
 def main():
     
-    # Define text file
+    #define text file
     fileName = 'packet2.txt'
     data = dataImport(fileName)
     
     # Sort data into separate dataframes based on class label
     GPS_Data = data[data[0] == 1].drop([7], axis=1)
     RMC_Data = data[data[0] == 2].drop([7], axis=1)
-    ACC_Data = data[data[0] == 3].drop([5, 6, 7], axis=1)
+    ACC_Data = data[data[0] == 3].drop([0, 5, 6, 7], axis=1)
+    print(ACC_Data)
+    ACC_Data_M = ACC_Data.melt(1, var_name='Sensor_number', value_name='val')
+    print(ACC_Data_M)
     IMU_Data = data[data[0] == 4]
     TC_Data = data[data[0] == 5]
     PRES_Data = data[data[0] == 6].drop([7], axis=1)
     Spectrometer_Data = data[data[0] == 7].drop([7], axis=1)
     
     # graph stuff
-    graph = sb.relplot(data=ACC_Data, kind="line",x=1, y=3)
-    graph.set(title = "Temperature Data", xlabel='time',ylabel='temp')
+    graph = sb.lineplot(x=1,y='val',hue='Sensor_number', data=ACC_Data_M)
+    #Graph1 = sb.lineplot(x=1, y= [2,3,4], data=ACC_Data)
+    #graph = sb.relplot(data=ACC_Data, kind="line",x=1, y=2)
+   # graph = sb.relplot(data=ACC_Data, kind="line",x=1, y=3)
+    #graph = sb.relplot(data=ACC_Data, kind="line",x=1, y=4)
+    #graph.set(title = "Temperature Data", xlabel='time',ylabel='temp')
 main()
