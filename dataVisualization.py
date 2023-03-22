8ed09fb11545658d9210882c52dcee4cf6e18e1c
mattsak <87535803+mattsak@users.noreply.github.com>
2023-03-22 15:28:35 -0400

    Update dataVisualization.py

--- dataVisualization.py
+++ dataVisualization.py
@@ -1,1 +1,18 @@
+import seaborn as sb
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+def dataImport(textfile):
+    df = pd.read_csv(textfile, sep=" ", header=None)
+    #df = df.iloc[1:]
+    return df
+    
+    
+def main():
+    #fileName = input('Enter filename: ')
+    fileName = 'packet1.txt'
+    data = dataImport(fileName)
+    print(data)
+
+main()
