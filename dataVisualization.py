9cceb3bb6348735a0f2c36e0b9f88cd4a7253611
Sam Adkins <samadkins360@gmail.com>
2023-04-06 14:18:05 -0400

    Update to catch alec up
    ....

--- dataVisualization.py
+++ dataVisualization.py
@@ -3,8 +3,6 @@ Linux Commands:
 
 g++ decode.cpp -o decode
 ./decode output.dat
-
-
 '''
 
 # Import Pandas Library to create a data frame and seaborn library for visualization tools
@@ -13,22 +11,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# Reads a text file, returns a data frame without header and footer
+# Reads a text file, returns a data frame
 def dataImport(textfile):
-    df = pd.read_csv(textfile, sep=",", header=None, names=list(range(8)), engine="python")
+    df = pd.read_csv(textfile, sep=",", header=None, names=list(range(10)), engine="python")
     return df
 
 def main():
     # ask for type of input
     choice = input("Would you like to plot decoded packets or data? (packets/data): ")
+
+    # Packet input
     if (choice == "packets"):
-        # define text file
+        # See if path exists
         if (not Path("decodedpackets").exists()):
             print("Folder \"decodedpackets\" not found.")
             return
 
-        GPS_Data = pd.DataFrame() # unused for now
-        RMC_Data = pd.DataFrame() # unused for now
+        # Create blank dataframes
+        GPS_Data = pd.DataFrame() 
+        RMC_Data = pd.DataFrame()
         ACC_Data = pd.DataFrame()
         IMU_Data = pd.DataFrame()
         TC_Data = pd.DataFrame()
@@ -40,9 +41,7 @@ def main():
         files = Path("decodedpackets").glob('*')
         for file in files:
             fileCount += 1
-        
         graphCount = input("How many packets should be graphed? (There are " + str(fileCount) + " total packets): ")
-        
         try:
             graphCount = int(graphCount)
         except ValueError:
@@ -52,7 +51,7 @@ def main():
             print("That was not acceptable input.")
             return
 
-        # import the data from each file in directory
+        # import the data from each packet file in directory
         files = Path("decodedpackets").glob('*')
         for file in files:
             x = str(file)
@@ -63,34 +62,37 @@ def main():
                 print("Adding " + str(file) + " to the graph.")
                 data = dataImport(file)
 
-                #new_GPS_Data = data[data[0] == 1].drop([0], axis=1) # unused for now
-                #GPS_Data = pd.concat([GPS_Data, new_GPS_Data]) # unused for now
+                new_GPS_Data = data[data[0] == 1].drop([0, 2, 3, 4, 5, 8, 9], axis=1) 
+                GPS_Data = pd.concat([GPS_Data, new_GPS_Data])
 
-                #new_RMC_Data = data[data[0] == 2].drop([0], axis=1) # unused for now
-                #RMC_Data = pd.concat([RMC_Data, RMC_GPS_Data]) # unused for now
+                new_RMC_Data = data[data[0] == 2].drop([0, 2, 3, 4, 5, 8, 9], axis=1)
+                RMC_Data = pd.concat([RMC_Data, new_RMC_Data])
 
-                new_ACC_Data = data[data[0] == 3].drop([0, 5, 6, 7], axis=1)
+                new_ACC_Data = data[data[0] == 3].drop([0, 5, 6, 7, 8, 9], axis=1)
                 ACC_Data = pd.concat([ACC_Data, new_ACC_Data])
 
-                new_IMU_Data = data[data[0] == 4].drop([0], axis=1)
+                new_IMU_Data = data[data[0] == 4].drop([0, 8, 9], axis=1)
                 IMU_Data = pd.concat([IMU_Data, new_IMU_Data])
                 
-                new_TC_Data = data[data[0] == 5].drop([0], axis=1)
+                new_TC_Data = data[data[0] == 5].drop([0, 8, 9], axis=1)
                 TC_Data = pd.concat([TC_Data, new_TC_Data])
 
-                new_PRES_Data = data[data[0] == 6].drop([0, 7], axis=1)
+                new_PRES_Data = data[data[0] == 6].drop([0, 7, 8, 9], axis=1)
                 PRES_Data = pd.concat([PRES_Data, new_PRES_Data])
 
                 #new_Spectrometer_Data = data[data[0] == 7].drop([0], axis=1) # unused for now
                 #Spectrometer_Data = pd.concat([Spectrometer_Data, new_Spectrometer_Data]) # unused for now
+
+    # Data input
     elif(choice == "data"):
-        # define text file
+        # See if path exists
         if (not Path("decodeddata").exists()):
             print("Folder \"decodeddata\" not found.")
             return
 
-        GPS_Data = pd.DataFrame() # unused for now
-        RMC_Data = pd.DataFrame() # unused for now
+        # Create blank dataframes
+        GPS_Data = pd.DataFrame()
+        RMC_Data = pd.DataFrame()
         ACC_Data = pd.DataFrame()
         IMU_Data = pd.DataFrame()
         TC_Data = pd.DataFrame()
@@ -99,65 +101,65 @@ def main():
 
         # Take input for number of packets to be graphed
         print("Here are the available files:")
-    
         files = Path("decodeddata").glob('*')
         for file in files:
             print(str(file))
-
         fileChoice = input("Would you like all files graphed or one in particular? (all/\"name_of_file\"): ")
         
+        # All types of data to be plotted
         if (fileChoice == "all"):
-            # import the data from each file in directory
+            # import the data from each data file in directory
             files = Path("decodeddata").glob('*')
             for file in files:
                 print("Adding " + str(file) + " to the graph...")
                 data = dataImport(file)
 
-                #new_GPS_Data = data[data[0] == 1].drop([0], axis=1) # unused for now
-                #GPS_Data = pd.concat([GPS_Data, new_GPS_Data]) # unused for now
+                new_GPS_Data = data[data[0] == 1].drop([0, 2, 3, 4, 5, 8, 9], axis=1) 
+                GPS_Data = pd.concat([GPS_Data, new_GPS_Data])
 
-                #new_RMC_Data = data[data[0] == 2].drop([0], axis=1) # unused for now
-                #RMC_Data = pd.concat([RMC_Data, RMC_GPS_Data]) # unused for now
+                new_RMC_Data = data[data[0] == 2].drop([0, 2, 3, 4, 5, 8, 9], axis=1)
+                RMC_Data = pd.concat([RMC_Data, new_RMC_Data])
 
-                new_ACC_Data = data[data[0] == 3].drop([0], axis=1)
+                new_ACC_Data = data[data[0] == 3].drop([0, 5, 6, 7, 8, 9], axis=1)
                 ACC_Data = pd.concat([ACC_Data, new_ACC_Data])
 
-                new_IMU_Data = data[data[0] == 4].drop([0], axis=1)
+                new_IMU_Data = data[data[0] == 4].drop([0, 8, 9], axis=1)
                 IMU_Data = pd.concat([IMU_Data, new_IMU_Data])
                 
-                new_TC_Data = data[data[0] == 5].drop([0], axis=1)
+                new_TC_Data = data[data[0] == 5].drop([0, 8, 9], axis=1)
                 TC_Data = pd.concat([TC_Data, new_TC_Data])
 
-                new_PRES_Data = data[data[0] == 6].drop([0], axis=1)
+                new_PRES_Data = data[data[0] == 6].drop([0, 7, 8, 9], axis=1)
                 PRES_Data = pd.concat([PRES_Data, new_PRES_Data])
 
                 #new_Spectrometer_Data = data[data[0] == 7].drop([0], axis=1) # unused for now
                 #Spectrometer_Data = pd.concat([Spectrometer_Data, new_Spectrometer_Data]) # unused for now
 
+        # Specific type of data to get plotted alone
         elif (Path(fileChoice).exists()):
-            # import the data from each file in directory
+            # import the data from each data file in directory
             files = Path("decodeddata").glob('*')
             for file in files:
                 if (str(file) == fileChoice):
                     print("Adding " + str(file) + " to the graph...")
                     data = dataImport(file)
 
-                    #new_GPS_Data = data[data[0] == 1].drop([0], axis=1) # unused for now
-                    #GPS_Data = pd.concat([GPS_Data, new_GPS_Data]) # unused for now
+                    new_GPS_Data = data[data[0] == 1].drop([0, 2, 3, 4, 5, 8, 9], axis=1) 
+                    GPS_Data = pd.concat([GPS_Data, new_GPS_Data])
 
-                    #new_RMC_Data = data[data[0] == 2].drop([0], axis=1) # unused for now
-                    #RMC_Data = pd.concat([RMC_Data, RMC_GPS_Data]) # unused for now
+                    new_RMC_Data = data[data[0] == 2].drop([0, 2, 3, 4, 5, 8, 9], axis=1)
+                    RMC_Data = pd.concat([RMC_Data, new_RMC_Data])
 
-                    new_ACC_Data = data[data[0] == 3].drop([0], axis=1)
+                    new_ACC_Data = data[data[0] == 3].drop([0, 5, 6, 7, 8, 9], axis=1)
                     ACC_Data = pd.concat([ACC_Data, new_ACC_Data])
 
-                    new_IMU_Data = data[data[0] == 4].drop([0], axis=1)
+                    new_IMU_Data = data[data[0] == 4].drop([0, 8, 9], axis=1)
                     IMU_Data = pd.concat([IMU_Data, new_IMU_Data])
                     
-                    new_TC_Data = data[data[0] == 5].drop([0], axis=1)
+                    new_TC_Data = data[data[0] == 5].drop([0, 8, 9], axis=1)
                     TC_Data = pd.concat([TC_Data, new_TC_Data])
 
-                    new_PRES_Data = data[data[0] == 6].drop([0], axis=1)
+                    new_PRES_Data = data[data[0] == 6].drop([0, 7, 8, 9], axis=1)
                     PRES_Data = pd.concat([PRES_Data, new_PRES_Data])
 
                     #new_Spectrometer_Data = data[data[0] == 7].drop([0], axis=1) # unused for now
@@ -168,7 +170,20 @@ def main():
                     TC_Data_M = TC_Data.melt(1, var_name='Sensor_number', value_name='val')
                     PRES_Data_M = PRES_Data.melt(1, var_name='Sensor_number', value_name='val')
 
+                    if (len(GPS_Data) != 0):
+                        # GPS Graph settings
+                        GPS_graph = sb.scatterplot(x=6,y=7, hue=1, data=GPS_Data, palette="deep")
+                        GPS_graph.set(title = "GPS Data", xlabel='latitude',ylabel='longitude', facecolor="#e0e0e0")
+                        GPS_graph.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+
+                    if (len(RMC_Data) != 0):
+                        # RMC Graph settings
+                        RMC_graph = sb.scatterplot(x=6,y=7, hue=1, data=RMC_Data, palette="deep")
+                        RMC_graph.set(title = "RMC Data", xlabel='latitude',ylabel='longitude', facecolor="#e0e0e0")
+                        RMC_graph.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+                
                     if (len(ACC_Data) != 0):
+                        # ACC Graph settings
                         ACC_graph = sb.lineplot(x=1,y='val',hue='Sensor_number', data=ACC_Data_M, marker='d')
                         ACC_graph.set(title = "Acceleration Data", xlabel='time',ylabel='acceleration',
                             facecolor="#e0e0e0")
@@ -196,46 +211,36 @@ def main():
                     plt.subplots_adjust(hspace=0.3, wspace=0.42)
                     plt.show()
                     return
-
+        
+        # Bad input
         else:
             print("That was not acceptable input.")
             return
 
+    # Bad input
     else:
         print("That was not acceptable input.")
         return
 
     # Melt/Format data correctly for graphs
-    #GPS_Data_M = GPS_Data.melt(1, var_name='Sensor_number', value_name='val') # unused for now
-    #RMC_Data_M = RMC_Data.melt(1, var_name='Sensor_number', value_name='val') # unused for now
-
     ACC_Data_M = ACC_Data.melt(1, var_name='Sensor_number', value_name='val')
     IMU_Data_M = IMU_Data.melt(1, var_name='Sensor_number', value_name='val')
     TC_Data_M = TC_Data.melt(1, var_name='Sensor_number', value_name='val')
     PRES_Data_M = PRES_Data.melt(1, var_name='Sensor_number', value_name='val')
-
-    #Spectrometer_Data_M = Spectrometer_Data.melt(1, var_name='Sensor_number', value_name='val') # unused for now
+    #Spectrometer_Data_M = Spectrometer_Data.melt(1, var_name='Sensor_number', value_name='val') # unused for now and needs additonal changing
 
     # Graph setup
-    fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(12,7), facecolor='#adadad')
+    fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(12,7), facecolor='#adadad')
 
-    #GPS Graph settings # unused for now and needs additional changing
-    '''
-    GPS_graph = sb.lineplot(x=1,y='val',hue='Sensor_number', data=GPS_Data_M, 
-        ax=axs[], marker='d')
-    GPS_graph.set(title = "GPS Data", xlabel='time',ylabel='',
-        facecolor="#e0e0e0")
+    #GPS Graph settings
+    GPS_graph = sb.scatterplot(x=6,y=7,hue=1, data=GPS_Data, ax=axs[0, 2], palette="deep")
+    GPS_graph.set(title = "GPS Data", xlabel='latitude',ylabel='longitude', facecolor="#e0e0e0")
     GPS_graph.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
-    '''
 
-    #RMC Graph settings # unused for now and needs additional changing
-    '''
-    RMC_graph = sb.lineplot(x=1,y='val',hue='Sensor_number', data=RMC_Data_M, 
-        ax=axs[], marker='d')
-    RMC_graph.set(title = "RMC Data", xlabel='time',ylabel='',
-        facecolor="#e0e0e0")
+    #RMC Graph settings
+    RMC_graph = sb.scatterplot(x=6,y=7, hue=1, data=RMC_Data, palette="deep")
+    RMC_graph.set(title = "RMC Data", xlabel='latitude',ylabel='longitude', facecolor="#e0e0e0")
     RMC_graph.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
-    '''
 
     # ACC Graph settings
     ACC_graph = sb.lineplot(x=1,y='val',hue='Sensor_number', data=ACC_Data_M, 
@@ -277,5 +282,6 @@ def main():
     # Show plots
     plt.subplots_adjust(hspace=0.3, wspace=0.42)
     plt.show()
+    return
     
 main()
